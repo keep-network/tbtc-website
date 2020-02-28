857b1963c3a5b7a930a3f5058dcffcb13084ab0e
Antonio Salazar Cardozo <antonio@thesis.co>
2020-02-28 08:04:18 -0500

    Add previews for news items
    The preview is the full news item page, header and footer included, for
    completeness and styling purposes. A few silly workarounds to deal with
    the fact that we have very different data from the CMS preview engine vs
    gatsby's build GraphQL, including some duplicated date format because
    it's impossible to interpolate constants into GraphQL. Whee!

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,6 +1,23 @@
 import CMS from 'netlify-cms-app'
 import uploadcare from 'netlify-cms-media-library-uploadcare'
 import cloudinary from 'netlify-cms-media-library-cloudinary'
+import ReactDOMServer from 'react-dom/server'
+
+import moment from 'moment'
+
+import "../css/app.scss"
+import { NewsItemTemplate } from '../templates/news-item'
+import App from '../components/App'
+
+const NewsItemPreview = ({ entry, widgetFor }) => {
+    return App({ children: NewsItemTemplate({
+        title: entry.getIn(["data", "title"]),
+        date: moment(widgetFor("date")).format("YYYY-MM-DD"),
+        description: widgetFor("description"),
+        body: ReactDOMServer.renderToStaticMarkup(widgetFor("body")),
+    }) })
+}
 
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
+CMS.registerPreviewTemplate("news-item", NewsItemPreview)
