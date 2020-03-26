a1168df91bad38270690608564c6e8689bcfc95e
Antonio Salazar Cardozo <antonio@thesis.co>
2020-03-26 18:54:17 -0400

    Add mobile nav menu functionality
    The nav menu is collapsed on mobile widths, expanding on hover of the
    entire header or on click of the navbar placeholder, which is only
    visible in smaller widths. The hit area for that placeholder is
    reasonably large, and the visual representation is two side-by-side
    dots.

--- src/components/lib/Header.js
+++ src/components/lib/Header.js
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import TBTCLogo from '../svgs/TBTCLogo'
 
-const Header = props => (
-  <nav className="nav">
+const Header = props => {
+  const [showMenu, setMenu] = useState(false)
+  const toggleMenu = () => {
+    setMenu(! showMenu)
+  }
+
+  let menuClass = "menu-label"
+  if (showMenu) {
+    menuClass += " open"
+  }
+
+  return <nav className="nav">
     <a className="logo" href="/">
       <TBTCLogo width="110" />
     </a>
 
     <ul>
+      <li className={menuClass} onClick={toggleMenu}><button>Navigation</button></li>
       <li><a href="https://docs.keep.network/tbtc/" target="_blank">Read Spec</a></li>
       <li><a href="/#mailing-list">Get Updates</a></li>
     </ul>
   </nav>
-)
+}
 
 export default Header
