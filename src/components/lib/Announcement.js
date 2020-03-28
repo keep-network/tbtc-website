74a758f31c7eb301bd9042d5213ff405c4c0e960
ironng <ironng@users.noreply.github.com>
2020-03-27 22:13:57 -0700

    Fix spacing

--- src/components/lib/Announcement.js
+++ src/components/lib/Announcement.js
@@ -2,17 +2,17 @@ import React from 'react'
 
 
 const Announcement = ({ children }) => (
-    <div className="announcement">
-        <div className="container">
-            <div className="row justify-content-center no-gutters">
-                <div className="col-10 col-sm-12 col-md-12 col-lg-10 content">
-                    <p>
-                        { children }
-                    </p>
-                </div>
-            </div>
+  <div className="announcement">
+    <div className="container">
+      <div className="row justify-content-center no-gutters">
+        <div className="col-10 col-sm-12 col-md-12 col-lg-10 content">
+          <p>
+            { children }
+          </p>
         </div>
+      </div>
     </div>
+  </div>
 )
 
 export default Announcement
--- src/components/lib/Spotlight.js
+++ src/components/lib/Spotlight.js
@@ -3,11 +3,11 @@ import classNames from 'classnames'
 
 
 const Spotlight = ({ children, className, sideLabel }) => (
-    <div className={classNames('spotlight', className)}>
-        <div className="side-label-left">{sideLabel}</div>
-        { children }
-        <div className="side-label-right">{sideLabel}</div>
-    </div>
+  <div className={classNames('spotlight', className)}>
+    <div className="side-label-left">{sideLabel}</div>
+      { children }
+    <div className="side-label-right">{sideLabel}</div>
+  </div>
 )
 
 export default Spotlight
