b44b258197df146c9ec7e2dcacf099bf71ccad62
ironng <ironng@users.noreply.github.com>
2020-04-01 15:14:05 -0700

    Add cms preview for faq page

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -7,6 +7,7 @@ import moment from 'moment'
 
 import "../css/app.scss"
 import { AboutPageTemplate } from '../templates/about-page'
+import { FaqPageTemplate } from '../templates/faq-page'
 import { NewsItemTemplate } from '../templates/news-item'
 import App from '../components/App'
 
@@ -22,6 +23,16 @@ const AboutPagePreview = ({ entry, widgetFor }) => {
     }) })
 }
 
+const FaqPagePreview = ({ entry }) => {
+    const entryQuestions = entry.getIn(["data", "questions"])
+    const questions = entryQuestions ? entryQuestions.toJS() : []
+
+    return App({ children: FaqPageTemplate({
+        title: entry.getIn(["data", "title"]),
+        questions: questions
+    }) })
+}
+
 const NewsItemPreview = ({ entry, widgetFor }) => {
     return App({ children: NewsItemTemplate({
         title: entry.getIn(["data", "title"]),
@@ -34,4 +45,5 @@ const NewsItemPreview = ({ entry, widgetFor }) => {
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
 CMS.registerPreviewTemplate("about-page", AboutPagePreview)
+CMS.registerPreviewTemplate("faq-page", FaqPagePreview)
 CMS.registerPreviewTemplate("news-item", NewsItemPreview)
