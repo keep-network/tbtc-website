e51f9975e37b9df92998f563e0b0114e951cd654
Antonio Salazar Cardozo <antonio@thesis.co>
2020-04-06 22:51:41 -0400

    Add SEO component and use it with overrides in App
    Overrides can simply pass title and description props to the App
    component when rendering; otherwise, defaults are used from the
    top-level Gatsby config.

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import { Announcement, Footer, Header, Newsletter } from './lib'
+import SEO from './SEO.js'
 
 import '../css/app.scss'
 
-
 export default (props) => {
-  const { children } = props
+  const { children, title, description } = props
 
   return (
     <div className="main">
       <Header />
+      <SEO title={title} description={description} />
       <Announcement>
         <Link to="/news/2020-02-14-ropsten">tBTC is now live on Ropsten</Link>. Mainnet launch is coming soon.
       </Announcement>
