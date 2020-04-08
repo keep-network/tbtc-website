12b48da6bee1ae16d7afd127ed03cb88c82a0f75
ironng <ironng@users.noreply.github.com>
2020-04-07 17:01:52 -0700

    Address eslint warnings

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'gatsby'
 import { Announcement, Footer, Header, Newsletter } from './lib'
 import SEO from './SEO.js'
 
@@ -13,7 +12,7 @@ export default (props) => {
       <Header />
       <SEO title={title} description={description} />
       <Announcement>
-        tBTC will launch on April 27th, 2020. Read more in <a href="https://www.bloomberg.com/news/articles/2020-04-02/bitcoin-s-ethereum-rivalry-could-be-assuaged-with-tbtc-bridge" target="_blank" rel="noreferrer">Bloomberg</a>.
+        tBTC will launch on April 27th, 2020. Read more in <a href="https://www.bloomberg.com/news/articles/2020-04-02/bitcoin-s-ethereum-rivalry-could-be-assuaged-with-tbtc-bridge" target="_blank" rel="noopener noreferrer">Bloomberg</a>.
       </Announcement>
       <div className="app">
         { children }
