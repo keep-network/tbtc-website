bd0f2b52c4f315d8dc4a13a29dd4fd9a08526b0f
ironng <ironng@users.noreply.github.com>
2020-04-13 13:16:59 -0700

    Add StaticQuery to Announcement component
    Component returns null if no data is available

--- src/components/lib/Announcement.js
+++ src/components/lib/Announcement.js
@@ -1,18 +1,44 @@
 import React from 'react'
+import { graphql, StaticQuery } from 'gatsby'
 
 
-const Announcement = ({ children }) => (
+const Announcement = ({ body }) => (
   <div className="announcement">
     <div className="container">
       <div className="row justify-content-center no-gutters">
         <div className="col-10 col-sm-12 col-md-12 col-lg-10 content">
-          <p>
-            { children }
-          </p>
+          <div className="body" dangerouslySetInnerHTML={{ __html: body }} />
         </div>
       </div>
     </div>
   </div>
 )
 
-export default Announcement
+
+// Query for announcement
+export const query = graphql`
+  query Announcement {
+    allMarkdownRemark(
+      filter: { frontmatter: { template: { eq: "announcement" } } }
+    ) {
+      edges {
+        node {
+          html
+        }
+      }
+    }
+  }
+`
+
+export default () => (
+  <StaticQuery
+    query={query}
+    render={data => {
+      const body = data.allMarkdownRemark.edges[0].node.html
+      if (!body) {
+        return null
+      }
+      return <Announcement body={body} />
+    }}
+  />
+)
