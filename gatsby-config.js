fc480d62c5d6525dcd9667ea9bf3688f8ff4699c
Matt Luongo <mhluongo@gmail.com>
2020-04-20 16:43:00 -0400

    Support languages specified by md file suffix
    References #62.

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,9 @@ module.exports = {
     titleTemplate: "tBTC – %s",
     description: "1. Deposit BTC. 2. Mint TBTC. 3. Lend and earn interest on your BTC.",
     url: "https://tbtc.network",
+    // supported 639-1 language codes
+    supportedLanguages: ["en", "ru"],
+    defaultLanguage: "en",
   },
   plugins: [
     'gatsby-plugin-react-helmet',
