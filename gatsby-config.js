d75bc5e01aadd017fa64d04cf2bbabbe868ae232
Matt Luongo <mhluongo@gmail.com>
2020-07-20 16:34:03 -0400

    Alphabetize locale listings

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,8 @@ module.exports = {
     description: "1. Deposit BTC. 2. Mint TBTC. 3. Lend and earn interest on your BTC.",
     url: "https://tbtc.network",
     // supported 639-1 language codes
-    supportedLocales: ["en", "ru", "de", "es", "fr", "pl", "ja", "vn", "id", "zh", "ko"],
+    supportedLocales: [
+      "en", "de", "es", "fr", "id", "ja", "ko", "pl", "ru", "vn", "zh"],
     defaultLocale: "en",
   },
   plugins: [
