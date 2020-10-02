63100ed1d8e499343a2e3fe2e2d1c8f8799b3f1c
Antonio Salazar Cardozo <antonio@thesis.co>
2020-10-02 13:51:11 -0400

    Slap home-page.js around with the prettier trout
    Only formatting changes here, not to worry!

--- src/templates/home-page.js
+++ src/templates/home-page.js
@@ -1,11 +1,10 @@
-import React from 'react'
-import { graphql } from 'gatsby'
-import PropTypes from 'prop-types'
+import React from "react"
+import { graphql } from "gatsby"
+import PropTypes from "prop-types"
 
-import { App } from '../components'
-import { Image, Integrations } from '../components/lib'
-import SandDollar from '../components/svgs/SandDollar'
-import Link from '../components/LocaleLink'
+import { App } from "../components"
+import { Image, Integrations } from "../components/lib"
+import Link from "../components/LocaleLink"
 
 export const HomePageTemplate = ({
   hero = {},
@@ -14,11 +13,12 @@ export const HomePageTemplate = ({
   spotlight2 = {},
   newsItems = [],
   resources = [],
-  integrations = {}
+  integrations = {},
 }) => {
-  return <div className="home">
-    <div className="container">
-      <div className="row justify-content-center no-gutters">
+  return (
+    <div className="home">
+      <div className="container">
+        <div className="row justify-content-center no-gutters">
         <section className="hero col-sm-12 col-md-12 col-lg-10">
           <div className="row">
             <div className="col-sm-12 col-md-6">
@@ -46,92 +46,119 @@ export const HomePageTemplate = ({
               <SandDollar className="sand-dollar-hero" />
             </div>
           </div>
-        </section>
-        <section className="features col-sm-12 col-md-12 col-lg-10">
-          <ol className="row">
-            { features && features.map((feature, i) => (
-              <li key={`feature-${i}`}
-                className="col-12 col-sm-12 col-md-6 col-xl-3">
-                <h2>{feature.title}</h2>
-                <p>{feature.description}</p>
-              </li>
-            )) }
-          </ol>
-        </section>
-      </div>
-    </div>
-    <section className="billboard">
-      <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-sm-12 col-md-12 col-lg-10">
-            <h1>
-              {spotlight1.title}
-            </h1>
-            <div className="body" dangerouslySetInnerHTML={{ __html: spotlight1.body }}/>
-            {spotlight1.button ? (
-              <a className="billboard-button-link"
-                href={spotlight1.button.url} target="_blank" rel="noopener noreferrer">
-                {spotlight1.button.text}
-              </a>
-            ) : ''}
-          </div>
+          </nav>
+          <section className="features col-sm-12 col-md-12 col-lg-10">
+            <ol className="row">
+              {features &&
+                features.map((feature, i) => (
+                  <li
+                    key={`feature-${i}`}
+                    className="col-12 col-sm-12 col-md-6 col-xl-3"
+                  >
+                    <h2>{feature.title}</h2>
+                    <p>{feature.description}</p>
+                  </li>
+                ))}
+            </ol>
+          </section>
         </div>
       </div>
-    </section>
-    <div className="container">
-      <div className="row justify-content-center no-gutters">
-        <section className="latest-news col-sm-12 col-md-12 col-lg-10">
-          <h1 className="section-title">Latest News</h1>
-          <div className="row">
-            { newsItems && newsItems.map(({ node }) => (
-              <div className="latest-news-item col-sm-12 col-lg-4" key={node.id}>
-                <div className="date">{node.frontmatter.date}</div>
-                <h2>{node.frontmatter.title}</h2>
-                <p>{node.excerpt}</p>
-                <Link locale={node.fields.locale} to={`${node.fields.slug}`}>Read more</Link>
-              </div>
-            ))}
-          </div>
-        </section>
-      </div>
-    </div>
-    <section className={`tailored-cta ${spotlight2.align}`}>
-      <div className="container">
-        <div className="side-label">{spotlight2.label}</div>
-        <div className="row">
-          <div className="tailored-cta-content col-sm-12 col-md-12 col-lg-8">
-            <h2>{spotlight2.title}</h2>
-            <a href={spotlight2.button.url}>
-              {spotlight1.button.text}
-            </a>
-          </div>
-          <div className="tailored-cta-image col-sm-12 col-md-12 col-lg-4">
-            <Image imageData={spotlight2.image} />
+      <section className="billboard">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-sm-12 col-md-12 col-lg-10">
+              <h1>{spotlight1.title}</h1>
+              <div
+                className="body"
+                dangerouslySetInnerHTML={{ __html: spotlight1.body }}
+              />
+              {spotlight1.button ? (
+                <a
+                  className="billboard-button-link"
+                  href={spotlight1.button.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {spotlight1.button.text}
+                </a>
+              ) : (
+                ""
+              )}
+            </div>
           </div>
         </div>
+      </section>
+      <div className="container">
+        <div className="row justify-content-center no-gutters">
+          <section className="latest-news col-sm-12 col-md-12 col-lg-10">
+            <h1 className="section-title">Latest News</h1>
+            <div className="row">
+              {newsItems &&
+                newsItems.map(({ node }) => (
+                  <div
+                    className="latest-news-item col-sm-12 col-lg-4"
+                    key={node.id}
+                  >
+                    <div className="date">{node.frontmatter.date}</div>
+                    <h2>{node.frontmatter.title}</h2>
+                    <p>{node.excerpt}</p>
+                    <Link
+                      locale={node.fields.locale}
+                      to={`${node.fields.slug}`}
+                    >
+                      Read more
+                    </Link>
+                  </div>
+                ))}
+            </div>
+          </section>
+        </div>
       </div>
-    </section>
-    <div className="container">
-      <div className="row justify-content-center no-gutters">
-        <section className="resources col-sm-12 col-md-12 col-lg-10">
-          <h1 className="section-title">Build with tBTC</h1>
+      <section className={`tailored-cta ${spotlight2.align}`}>
+        <div className="container">
+          <div className="side-label">{spotlight2.label}</div>
           <div className="row">
-            { resources && resources.map(({ node }) => (
-              <div className="resource-item col-sm-12 col-md-4" key={node.id}>
-                <h2>{node.frontmatter.title}</h2>
-                <p>{node.excerpt}</p>
-                <Link locale={node.fields.locale} to={`${node.fields.slug}`}>Go</Link>
-              </div>
-            ))}
+            <div className="tailored-cta-content col-sm-12 col-md-12 col-lg-8">
+              <h2>{spotlight2.title}</h2>
+              <a href={spotlight2.button.url}>{spotlight1.button.text}</a>
+            </div>
+            <div className="tailored-cta-image col-sm-12 col-md-12 col-lg-4">
+              <Image imageData={spotlight2.image} />
+            </div>
           </div>
-        </section>
-        <section className="integrations col-sm-12 col-md-12 col-lg-10">
-          <h1 className="section-title">{integrations.title}</h1>
-          <Integrations />
-        </section>
+        </div>
+      </section>
+      <div className="container">
+        <div className="row justify-content-center no-gutters">
+          <section className="resources col-sm-12 col-md-12 col-lg-10">
+            <h1 className="section-title">Build with tBTC</h1>
+            <div className="row">
+              {resources &&
+                resources.map(({ node }) => (
+                  <div
+                    className="resource-item col-sm-12 col-md-4"
+                    key={node.id}
+                  >
+                    <h2>{node.frontmatter.title}</h2>
+                    <p>{node.excerpt}</p>
+                    <Link
+                      locale={node.fields.locale}
+                      to={`${node.fields.slug}`}
+                    >
+                      Go
+                    </Link>
+                  </div>
+                ))}
+            </div>
+          </section>
+          <section className="integrations col-sm-12 col-md-12 col-lg-10">
+            <h1 className="section-title">{integrations.title}</h1>
+            <Integrations />
+          </section>
+        </div>
       </div>
     </div>
-  </div>
+  )
 }
 
 const HomePage = ({ data, pageContext }) => {
@@ -148,7 +175,8 @@ const HomePage = ({ data, pageContext }) => {
         spotlight2={post.frontmatter.spotlight_2}
         newsItems={newsItems}
         resources={resources}
-        integrations={post.frontmatter.integrations_section} />
+        integrations={post.frontmatter.integrations_section}
+      />
     </App>
   )
 }
@@ -161,8 +189,8 @@ HomePage.propTypes = {
     }),
     latestResources: PropTypes.shape({
       edges: PropTypes.array,
-    })
-  })
+    }),
+  }),
 }
 
 export default HomePage
@@ -233,9 +261,12 @@ export const query = graphql`
     }
 
     featuredNews: allMarkdownRemark(
-      limit: 3,
-      sort: {order: DESC, fields: [frontmatter___date]},
-      filter: {frontmatter: {template: {eq: "news-item"}, tags: {eq: "featured"}}, fields: {locale: {eq: $locale}}}
+      limit: 3
+      sort: { order: DESC, fields: [frontmatter___date] }
+      filter: {
+        frontmatter: { template: { eq: "news-item" }, tags: { eq: "featured" } }
+        fields: { locale: { eq: $locale } }
+      }
     ) {
       edges {
         node {
@@ -254,9 +285,12 @@ export const query = graphql`
     }
 
     latestResources: allMarkdownRemark(
-      limit: 3,
-      sort: {order: DESC, fields: [frontmatter___date]},
-      filter: {frontmatter: {template: {eq: "resource"}}, fields: {locale: {eq: $locale}}}
+      limit: 3
+      sort: { order: DESC, fields: [frontmatter___date] }
+      filter: {
+        frontmatter: { template: { eq: "resource" } }
+        fields: { locale: { eq: $locale } }
+      }
     ) {
       edges {
         node {
